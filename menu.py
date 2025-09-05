8de3fc248a45796c3f7af336610c52aa0804d21d
NOZZAJ777 <jnossapoveda10@gmail.com>
2025-09-05 13:43:50 -0500

    Delete menu.py

--- menu.py
+++
@@ -1,7 +0,0 @@
-print ("bienvenido al menu de la calculadora") 
-def calcualdora():
-        print("digite 1 si desea sumar")
-        print("digite 2 si desea restar")
-        print("digite 3 si desea multiplicacion")
-        print("digite 4 se desea dividir")
-opcion = (input ("Escoja una opcion: "))
